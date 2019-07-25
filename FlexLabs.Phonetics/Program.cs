7cd1b8a239520b7eb3b1e36ea1373a7d54aad723
Artiom Chilaru <artiom@flexlabs.org>
2019-07-25 14:24:30 +0100

    * Fixing the name in the typo + Adding Docker support

--- FlexLabs.Phonetics/Program.cs
+++ FlexLabs.Phonetics/Program.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 
-namespace FlexLabs.Phonerics
+namespace FlexLabs.Phonetics
 {
     public class Program
     {
--- FlexLabs.Phonetics/Startup.cs
+++ FlexLabs.Phonetics/Startup.cs
@@ -2,7 +2,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace FlexLabs.Phonerics
+namespace FlexLabs.Phonetics
 {
     public class Startup
     {
